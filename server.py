34318c1e3eaa44da177d68aa76e787ee19ecc795
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-03 22:04:16 +0200

    Implemented the fetch-a-tweet functionality.

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import configparser
 import json
+import os
 
-from flask import Flask, render_template
+import tweepy
+from flask import Flask, render_template, request
 
 from search import search
 from utils import read
@@ -30,6 +33,55 @@ def index():
     return render_template('index.html', rumours=rumours)
 
 
+@app.route("/fetch")
+def fetch_tweet_page():
+    return render_template('fetch.html')
+
+
+@app.route("/fetch-tweet", methods=['POST'])
+def fetch_tweet_by_id():
+    tweet_id = request.form['tweet_id']
+    create_folders(tweet_id)
+    api = authenticate()
+
+    tweet = api.get_status(tweet_id)
+    persist_tweet(tweet, "source-tweet")
+
+    return render_template('success.html')
+
+
+def create_folders(tweet_id):
+    tweet_id_as_string = str(tweet_id)
+    tweet_dir = "resources/dataset/rumoureval-data/random-rumours/" + tweet_id_as_string
+    src_tweet_dir = tweet_dir + "/source-tweet"
+    replies_tweet_dir = tweet_dir + "/replies"
+
+    os.makedirs(tweet_dir)
+    os.makedirs(src_tweet_dir)
+    os.makedirs(replies_tweet_dir)
+
+
+def authenticate():
+    config = configparser.ConfigParser()
+    config.read('config/twitter.ini')
+
+    consumer_key = config.get('Twitter', 'consumer_key')
+    consumer_secret = config.get('Twitter', 'consumer_secret')
+    access_key = config.get('Twitter', 'access_key')
+    access_secret = config.get('Twitter', 'access_secret')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    return tweepy.API(auth)
+
+
+def persist_tweet(tweet, folder):
+    tweet_id_as_string = str(tweet.id)
+    tweet_dir = "resources/dataset/rumoureval-data/random-rumours/" + tweet_id_as_string + "/" + folder
+    with open(tweet_dir + "/" + tweet_id_as_string + ".json", 'w') as outfile:
+        json.dump(tweet._json, outfile)
+
+
 @app.route("/style.css")
 def style():
     return render_template('style.css')
@@ -43,7 +95,7 @@ def search_text(text):
                    "rating": rating,
                    "text": threads[id]['source']['text'],
                    "replies": replies(threads[id])
-               # [tweet['text'] for tweet in threads[id].get('replies', dict()).values()]
+                   # [tweet['text'] for tweet in threads[id].get('replies', dict()).values()]
                } for id, rating in results]
     return json.dumps(results)
 
@@ -51,13 +103,13 @@ def search_text(text):
 @app.route('/rumour/<rumour>')
 def search_rumour(rumour):
     results = data[rumour].values()
-    print('dasd')
+    print('-----')
     print(len(results))
     print(len(list(results)))
     print(len(list(results)[0]))
     print(list(results)[0]['source']['text'])
     # print(json.dumps(list(results)[0]))
-    print('dasddsa')
+    print('-----')
     results = [{
                    "text": thread['source']['text'],
                    "replies": replies(thread)
