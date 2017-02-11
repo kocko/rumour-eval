282302c1415a9a2c02f5f884bf7d7a60162ca111
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-11 14:07:31 +0200

    Initial implementation on the feature_extractor.

--- feature/feature_extractor.py
+++ feature/feature_extractor.py
@@ -1,14 +1,31 @@
+from utils import read, write, folders, files
+
+from feature.contains_original import contains_original
+from feature.questionmark import contains_question_mark
+
+
+REPLIES = "..\\data\\tweets.json"
+OUTFILE = "..\\data\\vector.json"
+
+
 def main():
-    #open the tweet.json file
-    #for each tweet:
-    #(try to) extract each of the features from the tweet
-    #write json as:
-    # { 
-    #   id: tweet['id'],
-    #   vector: [contains_original, positive_words_percentage, negative_words_percentage, reversed_word_order, contains_question_mark],
-    #   rumour: tweet['rumour']
-    # }
-    print("hello")
+    data = read(REPLIES)
+    result = {}
+    for tweet in data:
+        tweet_id = tweet['id']
+        text = tweet['text']
+        if "reply_to" in tweet:
+            in_reply_to = tweet['reply_to']
+            vector = list()
+            vector.append(contains_original(text, in_reply_to))
+            vector.append(contains_question_mark(text))
+            result[tweet_id] = {
+                'rumour': tweet['rumour'],
+                'vector': vector
+            }
+
+    write(OUTFILE, result)
+
 
 if __name__ == "__main__":
     main()
