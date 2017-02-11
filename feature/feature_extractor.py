529ef1b3d905f48e7e019fbcfd7d9adc12d5d47e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-11 14:34:32 +0200

    Combined the features into the feature_extractor.py script.

--- feature/feature_extractor.py
+++ feature/feature_extractor.py
@@ -1,29 +1,34 @@
-from utils import read, write, folders, files
-
 from feature.contains_original import contains_original
+from feature.opinion_words import opinion_words_percentage
 from feature.questionmark import contains_question_mark
-
+from utils import read, write
 
 REPLIES = "..\\data\\tweets.json"
 OUTFILE = "..\\data\\vector.json"
+NEGATIVE_LEXICON = "..\\resources\\lexicon\\negative.txt"
+POSITIVE_LEXICON = "..\\resources\\lexicon\\positive.txt"
 
 
 def main():
     data = read(REPLIES)
+    negative = [line.rstrip('\n') for line in open(NEGATIVE_LEXICON)]
+    positive = [line.rstrip('\n') for line in open(POSITIVE_LEXICON)]
     result = {}
     for tweet in data:
         tweet_id = tweet['id']
         text = tweet['text']
+        vector = list()
         if "reply_to" in tweet:
             in_reply_to = tweet['reply_to']
-            vector = list()
             vector.append(contains_original(text, in_reply_to))
-            vector.append(contains_question_mark(text))
-            result[tweet_id] = {
-                'rumour': tweet['rumour'],
-                'vector': vector
-            }
-
+        vector.append(opinion_words_percentage(text, positive))
+        vector.append(opinion_words_percentage(text, negative))
+        # todo: implement reversed_word_order feature
+        vector.append(contains_question_mark(text))
+        result[tweet_id] = {
+            'rumour': tweet['rumour'],
+            'vector': vector
+        }
     write(OUTFILE, result)
 
 
