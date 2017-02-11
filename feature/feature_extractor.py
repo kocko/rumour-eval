6915bdc5697d7d64d010d4bdda3ea810ff766fd2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-11 13:14:32 +0200

    Added some comment just for my own usage.

---
+++ feature/feature_extractor.py
@@ -0,0 +1,14 @@
+def main():
+    #open the tweet.json file
+    #for each tweet:
+    #(try to) extract each of the features from the tweet
+    #write json as:
+    # { 
+    #   id: tweet['id'],
+    #   vector: [contains_original, positive_words_percentage, negative_words_percentage, reversed_word_order, contains_question_mark],
+    #   rumour: tweet['rumour']
+    # }
+    print("hello")
+
+if __name__ == "__main__":
+    main()
