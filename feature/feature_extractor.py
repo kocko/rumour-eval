df7c78c4e1cdbed898494e5c50284f1b702528c1
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-13 20:02:49 +0200

    Minor changes.

--- feature/feature_extractor.py
+++ feature/feature_extractor.py
@@ -1,5 +1,5 @@
 from feature.contains_original import contains_original
-from feature.opinion_words import opinion_words_percentage
+from feature.opinion_words import opinion_words_count
 from feature.questionmark import contains_question_mark
 from utils import read, write
 
@@ -10,7 +10,7 @@ POSITIVE_LEXICON = "..\\resources\\lexicon\\positive.txt"
 
 # vector explained:
 # [contains_original, positive_words_percentage, negative_words_percentage, reversed_word_order, contains_question_mark]
-# more possible features: contains_only_usernames
+
 
 def main():
     data = read(REPLIES)
@@ -24,8 +24,8 @@ def main():
         if "reply_to" in tweet:
             in_reply_to = tweet['reply_to']
             vector.append(contains_original(text, in_reply_to))
-        vector.append(opinion_words_percentage(text, positive))
-        vector.append(opinion_words_percentage(text, negative))
+        vector.append(opinion_words_count(text, positive))
+        vector.append(opinion_words_count(text, negative))
         # todo: implement reversed_word_order feature
         vector.append(contains_question_mark(text))
         result[tweet_id] = {
